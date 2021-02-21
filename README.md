d449eb11ffc54a923c0a651d848da718156ecec9
Steve-P42 <78639312+Steve-P42@users.noreply.github.com>
2021-02-21 21:17:08 +0100

    updated DSS methods

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DSS
 
+
 This is a decision support system to decide if a task should be done or not. 
 The variables based on which the DG Index is calculated are the due date and the task importance.
 DG Index = Decision Grading Index is a number between 0 and 1.
--- rework.py
+++ rework.py
@@ -37,6 +37,36 @@ class DSS:
     def __init__(self):
         self.today = datetime.date.today()
 
+    def calculate_time_multiplier(self, task):
+        if task.self.task_time <= 0.5:
+            t_multiplier = 0.95
+        elif 0.5 < task.self.task_time <= 1:
+            t_multiplier = 0.97
+        elif 1 < task.self.task_time <= 2:
+            t_multiplier = 0.99
+        elif 2 < task.self.task_time <= 3:
+            t_multiplier = 1
+        else:
+            t_multiplier = 1
+            print('You should consider splitting the task into subtasks. It takes too long.')
+        return t_multiplier
+
+    def answer(self, task):
+        print(f'\nThe task "{task.self.task_name}" will take {task.self.task_time} hour(s), has importance level {importance} and is due in \
+        {days} day(s).')
+        print(f'DG Index: {round(dg_index, 2)}')
+        if dg_index > 0.7:
+            print(f'The answer to the question: "Should you start this task ASAP?", is: YES, with \
+        {round(dg_index * 100, 2)}% relevance.')
+
+        if 0.5 <= dg_index <= 0.7:
+            print(f'The answer to the question: "Should you start this task ASAP?", is: There\'s probably something more \
+        important to do, with {round(dg_index * 100, 2)}% relevance.')
+
+        if dg_index < 0.5:
+            print(f'The answer to the question: "Should you start this task ASAP?", is: HECK NO, why are you even asking me?, \
+        with {round(dg_index * 100, 2)}% relevance.')
+
 # threading
 
 
